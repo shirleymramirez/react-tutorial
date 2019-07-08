1e9facf9f437554c1ff0eb66fe34d12cecafa3d6
shirley <shirley.mramirez@yahoo.com>
2019-07-08 11:11:07 -0700

    Updates

---
+++ precache-manifest.f4b1c0ddd6d82270932d1a1a1dd692ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfc066c71c71d04fc4db9ee08d1caf8",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2ac753fb0f7108eddf31",
+    "url": "/react-tutorial/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "2fa3868357dfd43b0526",
+    "url": "/react-tutorial/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2ac753fb0f7108eddf31",
+    "url": "/react-tutorial/static/js/2.2c16b26f.chunk.js"
+  },
+  {
+    "revision": "2fa3868357dfd43b0526",
+    "url": "/react-tutorial/static/js/main.947e2f09.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
